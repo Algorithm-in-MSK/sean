61a269c4ef4da0bbc342038d14b6bde9d204f817
sean-ahn <sean9509@gmail.com>
2018-06-28 22:22:52 +0900

    dp solved; recursion wip

--- 2011.py
+++ 2011.py
@@ -2,25 +2,53 @@ MAX_STR_LEN = 5000
 DIVIDE_BY = 1000000
 
 
+def is_start_with_non_zero(c):
+    return not c.startswith("0")
+
+
 def is_valid_code(c):
-    return 1 <= c <= 26
+    return is_start_with_non_zero(c) and 1 <= int(c) <= 26
 
 
-def count_interpretation(ipt, idx):
-    if idx == len(ipt) - 1:
+def recursion(ipt, idx):
+    if idx >= len(ipt) - 1:
         return 1
-    elif idx + 2 <= len(ipt) and is_valid_code(int(ipt[idx:idx+2])):
-        return count_interpretation(ipt, idx + 1) + count_interpretation(ipt, idx + 2)
+    elif idx + 2 <= len(ipt) and is_valid_code(ipt[idx:idx + 2]):
+        return recursion(ipt, idx + 1) + recursion(ipt, idx + 2)
     else:
-        if idx >= len(ipt) - 1:
-            return 1
-        return count_interpretation(ipt, idx + 1)
+        return recursion(ipt, idx + 1)
 
 
-def get_answer(q):
-    return count_interpretation(q, 0) % DIVIDE_BY
+def dynamic_programming(ipt):
+    length = len(ipt)
+    mem = [0 for _ in range(length)]
+    for idx in range(length):
+        if idx == 0:
+            mem[0] = int(is_valid_code(ipt[0]))
+        else:
+            if is_start_with_non_zero(ipt[idx]):
+                mem[idx] += mem[idx - 1]
+
+            # Trick: int(True) => 1, int(False) => 0
+            if idx == 1:
+                mem[idx] += int(is_valid_code(ipt[:2])) * 1
+            else:
+                mem[idx] += int(is_valid_code(ipt[idx - 1:idx + 1])) * mem[idx - 2]
+
+    return mem[length - 1]
+
+
+def validate_input(ipt):
+    assert ipt.isdigit() and len(ipt) <= MAX_STR_LEN
+
+
+def main(question):
+    validate_input(question)
+    # return recursion(question, 0) % DIVIDE_BY
+    return dynamic_programming(question) % DIVIDE_BY
 
 
 if __name__ == "__main__":
     question = input()
-    print(get_answer(question))
+    answer = main(question)
+    print(answer)
